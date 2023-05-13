9e8ce53938841849d71845c98a899c55761a9c64
pingProg <pingProg@github.com>
2023-05-13 11:19:43 +0800

    init：xposed环境

---
+++ app/src/main/java/com/ping/cloudmusicmod/Hook.java
@@ -0,0 +1,11 @@
+package com.ping.cloudmusicmod;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class Hook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+
+    }
+}
